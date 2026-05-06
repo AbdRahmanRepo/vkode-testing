b709552a90373ddb228ef38f1eeb800704a344dc
abdviome <abdul.rahman@viome.com>
2026-05-06 08:08:07 +0530

    added new folders

---
+++ main.ts
@@ -0,0 +1,21 @@
+/*
+function functionName() {
+    console.log("Hello World");
+}
+function with syntax
+*/
+
+// This is greet function
+function greet() {
+    console.log("Hello World");
+}
+
+// This is city function
+function city() {
+    console.log("This is chennai city");
+}
+
+//function call
+greet();
+city();
+
