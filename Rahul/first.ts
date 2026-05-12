0c49e0550f31b9df81e91255abc70716f90f0110
H-Rahul <rahulraall74@gmail.com>
2026-05-12 14:20:16 +0530

    Loop case's pratice

--- Rahul/first.ts
+++ Rahul/first.ts
@@ -1,46 +1,83 @@
-function Output (){
-    //console.log("Hi Team , You are")
+// function Output (){
+//     //console.log("Hi Team , You are")
 
-    let aa: number = 44;
-        if (aa==100)
-        {            //console.log("Excellent");
-        }else if (aa>=75){
-            //console.log("Good Marks");
-         } else if (aa>=50){
-            //console.log("Average");
-         }else {
-            //console.log("Failed in Exam");
-         }
+//     let aa: number = 44;
+//         if (aa==100)
+//         {            //console.log("Excellent");
+//         }else if (aa>=75){
+//             //console.log("Good Marks");
+//          } else if (aa>=50){
+//             //console.log("Average");
+//          }else {
+//             //console.log("Failed in Exam");
+//          }
 
     
-        }
+//         }
 
 
-Output()
-Output()
+// Output()
+// Output()
 
-function Result (Mark:number){
+// function Result (Mark:number){
 
-if (Mark<=35){
-   return("Failed")
-}else if (Mark>=35 && Mark<=50){
-   return("Average")
-}else if (Mark>=51 && Mark<=80){
-   return("Good")
-}else {return("Excellent")}
+// if (Mark<=35){
+//    return("Failed")
+// }else if (Mark>=35 && Mark<=50){
+//    return("Average")
+// }else if (Mark>=51 && Mark<=80){
+//    return("Good")
+// }else {return("Excellent")}
 
-}
+// }
 
 //console.log(Result(83));
 
-function chocolate (price: Number){
-if (price <= 10){
-   return"Kitkat";
-}else if (price >=11 && price <=50 ){
-   return "Milky bar";
-}else if (price >=51 && price <=100){
-   return "Dairy milk";
-}else {
-   return"No chocolate available";
-}
-} console.log (chocolate(102))
+//function chocolate (price: number){
+//if (price <= 10){
+//    return"Kitkat";
+// }else if (price >=11 && price <=50 ){
+//    return "Milky bar";
+// }else if (price >=51 && price <=100){
+//    return "Dairy milk";
+// }else {
+//    return"No chocolate available";
+// }
+// } //console.log (chocolate(102))
+
+//let c = 1, d=0;
+// while (c<=10){
+//    d=d+c
+//    c++
+// }
+// console.log("Sum is ", d)
+
+// do{
+// console.log("Number is", c);
+//    c++
+// }while(c<=10)
+
+// let s:number=0;
+
+// for(let f:number =1;f<=20;f++){
+//    if(f%2==0)
+//    {
+//       s=s+f;
+//    }
+// }
+// console.log("Sum of Even Number is", s)
+
+// for (let c:number=1;c<=20;c++){
+//    if(c%2==0){
+//       continue;
+//    }      console.log("Print Odd numbers", c);
+
+// }
+
+for (let c:number=1;c<=20;c++){
+   if(c%2==0){
+      console.log("Print Even number", c);
+     break;
+   }      
+
+ }
